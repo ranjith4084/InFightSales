78ecbadacb0965f7521d134a650566b878229171
ranjith4084 <velusamyranjith@gmail.com>
2021-12-31 17:47:10 +0530

    first commit

--- lib/core/extensions/enums.dart
+++
@@ -1,3 +0,0 @@
- enum State {
-  Idle,Besy
- }
--- lib/widget/custom_spacing.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-SizedBox HorizontalSpacing({double value: 5}) => SizedBox(
-      width: value,
-    );
-
-SizedBox VerticalSpacing({double value: 5}) => SizedBox(
-      height: value,
-    );
